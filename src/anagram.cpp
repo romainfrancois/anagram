de3a50886d0e7ba9a51ca5d0880a663b66094bbd
Romain Francois <romain@purrple.cat>
2018-03-10 18:17:29 +0100

    comment and finalise C++ code

--- src/anagram.cpp
+++ src/anagram.cpp
@@ -1,22 +1,53 @@
 #include <Rcpp.h>
 using namespace Rcpp;
+#include <array>
 
 // [[Rcpp::export]]
 std::vector<std::string> anagram_get( String word, List words ){
 
-  const char* s = word.get_cstring() ;
+  std::string s = word.get_cstring() ;
   int n = LENGTH(word.get_sexp()) ;
+
+  // counting how many times each letter from 'a' to 'z' appears in the word
+  std::array<int, 128> counts ;
+  std::fill( std::begin(counts), std::end(counts), 0 ) ;
+  for( int i=0; i<n; i++){
+    counts[ s[i] ]++ ;
+  }
+  std::array<bool,128> seen ;
+  std::fill( std::begin(seen), std::end(seen), false ) ;
 
   std::vector<std::string> results ;
 
   for( int i=0; i<n; i++){
     char c = s[i] ;
-    int idx = c - 'a' ;
+
+    // skip if we have already seen this as the first letter
+    if( seen[c] ) continue ;
+    seen[c] = true ;
 
     // words that start with the c character
-    CharacterVector all_words = words[idx] ;
+    // -'a' to align "a" with the first element of the list
+    CharacterVector all_words = words[c - 'a'] ;
 
-    results.push_back(CHAR(all_words[0].get())) ;
+    std::for_each( std::begin(all_words), std::end(all_words), [&counts, n, &results](auto st){
+      // copy the counts into a local array
+      std::array<int,128> local_counts ;
+      std::copy( std::begin(counts), std::end(counts), std::begin(local_counts) ) ;
+
+      // For every character in the current word, we
+      // decrement its count in the `local_counts` array
+      //
+      // if at any point we get a negative count it means
+      // that the current word is not an anagram of the target word
+      const char* w = CHAR(st) ;
+      for(int j=0; j<n; j++, w++){
+        if( --local_counts[*w] < 0 ) return ;
+      }
+
+      // if we survive the loop, we have an anagram
+      results.push_back( std::string(st) ) ;
+    }) ;
 
   }
 
